f01414b69ef4ffdeae1f3098904546b4a9fb43ca
Your Actual Name <Your Actual Email>
2019-01-01 00:08:15 +0900

    customized devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/show'
   root 'home#top'
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations' 
+  }
+  resources :users, only: %i(index show)
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
