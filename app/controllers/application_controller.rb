f1b57c7f3b268ec095d7720d7ddff30bfe841a78
Your Actual Name <Your Actual Email>
2019-01-09 22:52:32 +0900

    added icon_cache to the strong parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :icon])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :icon, :icon_cache])
   end
 end
