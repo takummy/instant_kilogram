ae22efefdaa39ef65880b68978d7d83eb04d7a4a
Your Actual Name <Your Actual Email>
2019-05-17 17:34:04 +0900

    modified carrierwave config to store cache in S3

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -12,12 +12,12 @@ class PicturesController < ApplicationController
   end
 
   def confirm
-    @picture = Picture.new(picture_params)
+    @picture = current_user.pictures.build(picture_params)
     render :new if @picture.invalid?
   end
 
   def create
-    @picture = Picture.new(picture_params)
+    @picture = current_user.pictures.build(picture_params)
     if @picture.save
       PostInfoMailer.notification_mail(@picture.user).deliver
       flash[:info] = '投稿完了'
@@ -60,7 +60,7 @@ class PicturesController < ApplicationController
   end
 
   def picture_params
-    params.require(:picture).permit(:image, :image_cache, :content).merge(user_id: current_user.id)
+    params.require(:picture).permit(:image, :image_cache, :content)
   end
 
   def correct_owner
